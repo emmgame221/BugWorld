d7a19302dfc8adfd3d965478df0e8a72b983d9bf
vbald <baldasv@sunypoly.edu>
2021-09-27 14:51:57 -0400

    as

---
+++ Button.cpp
@@ -0,0 +1,3 @@
+#include "Entity.h"
+#include "Button.h"
+#include "Game.h"
--- Button.h
+++ Button.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "Entity.h"
+#include <SFML/Graphics.hpp>
+
+class Button {
+    sf::FloatRect buttonRect;
+    
+};
