c81aa62d2fec3958af949905e5812954144f7eda
vbald <baldasv@sunypoly.edu>
2021-09-29 15:43:04 -0400

    Add and Subtract Functions

--- AddBug.h
+++ AddBug.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "Entity.h"
+#include <SFML/Graphics.hpp>
+
+class AddBug :
+	public Entity
+{
+public:
+	float scaleFactor;
+	int totalBug;
+	int bugCost;
+	int bugSellCost;
+	int food;
+	void Add();
+	void Subtract();
+	std::vector<sf::Sprite> spriteVector;
+
+};
+
