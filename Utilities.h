29aac3312c856140547c583a6a22ea67255be94d
Eric McHugh <emmgame221@gmail.com>
2021-10-06 11:47:47 -0700

    Actually changed Utilities.h

--- Utilities.h
+++ Utilities.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-#define PI 3.14159f
+constexpr float PI = 3.14159f;
 
-double angle(sf::Vector2f a, sf::Vector2f b) {
+float angle(sf::Vector2f a, sf::Vector2f b) {
 	return (atan2((b.y - a.y), (b.x - a.x)) * 180 / PI);
 }
 
@@ -12,6 +12,6 @@ sf::Vector2f normalize(sf::Vector2f vec) {
 	return sf::Vector2f(vec.x / mag, vec.y / mag);
 }
 
-double distance(sf::Vector2f a, sf::Vector2f b) {
+float distance(sf::Vector2f a, sf::Vector2f b) {
 	return sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2));
 }
