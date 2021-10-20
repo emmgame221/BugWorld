ec48d39728d8afa983c43c83c687e20010a0b79b
vbald <baldasv@sunypoly.edu>
2021-10-20 14:40:18 -0400

    Text loading

--- InfoMenu.cpp
+++ InfoMenu.cpp
@@ -5,6 +5,8 @@
 
   int Menu::bugMenu() {
     sf::RenderWindow window(sf::VideoMode(500, 500), "Info Menu");
+    infoText.setString(infoString);
+    loadInfo();
 
     while (window.isOpen())
     {
@@ -23,3 +25,22 @@
     }
     return 0;
 }
+
+  std::string Menu::loadInfo() {
+      font = sf::Font();
+      font.loadFromFile("./resources/NewYork.otf");
+      inputFile.open("./resources/BugInfo.txt");
+
+      while (!(inputFile.eof())) {
+          ch = inputFile.get();
+          infoString += ch;
+      }
+
+      infoText.setFont(font);
+      infoText.setString(infoString);
+      infoText.setFillColor(sf::Color::Black);
+      infoText.setPosition(sf::Vector2f(100.f, 100.f));
+      std::cout << infoString << std::endl;
+
+      return infoString;
+  }
