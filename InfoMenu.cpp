ae78c1f9e721f648702c2ef43fd314fa1eee1933
vbald <baldasv@sunypoly.edu>
2021-11-08 14:15:27 -0500

    InfoMenu Change

--- InfoMenu.cpp
+++ InfoMenu.cpp
@@ -1,16 +1,34 @@
 #include <SFML/Graphics.hpp>
-#include "Game.h"
 #include "Button.h"
 #include "InfoMenu.h"
 
   int Menu::bugMenu() {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "Info Menu");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Info Menu");
+    
     infoText.setString(infoString);
-    loadInfo();
+    window.draw(infoText);
+
+    font = sf::Font();
+    font.loadFromFile("./resources/NewYork.otf");
+    inputFile.open("./resources/BugInfo.txt");
+
+    while (!(inputFile.eof())) {
+        ch = inputFile.get();
+        infoString += ch;
+    }
+
+    infoText.setFont(font);
+    infoText.setString(infoString);
+    infoText.setFillColor(sf::Color::Black);
+    infoText.setPosition(sf::Vector2f(100.f, 100.f));
+    window.draw(infoText);
+    std::cout << infoString << std::endl;
 
     while (window.isOpen())
     {
         sf::Event event;
+        infoText.setString(infoString);
+        window.draw(infoText);
         while (window.pollEvent(event))
         {
             switch (event.type) {
@@ -20,27 +38,10 @@
 
                 window.clear(sf::Color::White);
                 window.display();
+                infoText.setString(infoString);
+                window.draw(infoText);
             }
         }
     }
     return 0;
 }
-
-  std::string Menu::loadInfo() {
-      font = sf::Font();
-      font.loadFromFile("./resources/NewYork.otf");
-      inputFile.open("./resources/BugInfo.txt");
-
-      while (!(inputFile.eof())) {
-          ch = inputFile.get();
-          infoString += ch;
-      }
-
-      infoText.setFont(font);
-      infoText.setString(infoString);
-      infoText.setFillColor(sf::Color::Black);
-      infoText.setPosition(sf::Vector2f(100.f, 100.f));
-      std::cout << infoString << std::endl;
-
-      return infoString;
-  }
